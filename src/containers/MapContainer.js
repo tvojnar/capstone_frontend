6acd45889dcb570248a252dfbadbfb85e67846a4
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-04 19:54:14 -0800

    props weren't passing from MapContainer to Map so I am just keeping the state and html rendering all in Map.js

---
+++ src/containers/MapContainer.js
@@ -0,0 +1,133 @@
+import { withGoogleMap, GoogleMap } from 'react-google-maps'
+import React, { Component } from 'react';
+import {HikeMarkerContainer} from '../containers/HikeMarkerContainer'
+import $ from 'jquery';
+import Map from '../components/Map';
+
+
+export class MapContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    // how zoomed in the map starts off as
+    this.zoom = 7
+
+    // store the map boundary coordinates
+    this.xMapBounds = { min: null, max: null }
+    this.yMapBounds = { min: null, max: null }
+
+    // start off with have mapFullyLoaded as false
+    this.mapFullyLoaded = false
+
+    // used to define the center point
+    // hikes will be all the hikes visable in a map area
+    // TODO: maybe be able to dynamically set the starting center point of the map?
+    this.state = {
+      lat: 46.6062,
+      lng: -122.3321,
+      hikes: [],
+    };
+  }
+
+  // called when the maps boundaries have changed
+  // calls three functions to change the map boundaties and center point and make an api call to get the hikes within those boundaries
+  handleMapChanged() {
+    this.getMapBounds()
+    this.setMapCenterPoint()
+    this.fetchHikesFromApi()
+  }
+
+
+  handleMapMounted(map) {
+    // assigns to the this.map value a map object during the map initialization
+    this.map = map
+  }
+
+
+  handleMapFullyLoaded() {
+    // marks that the map has fully loaded and calls the handleMapChanged function to get the lastest data
+    if (this.mapFullyLoaded)
+    return
+
+    this.mapFullyLoaded = true
+    this.handleMapChanged()
+  }
+
+  // this function gets and sets the center point
+  setMapCenterPoint() {
+    this.setState({
+      lat: this.map.getCenter().lat(),
+      lng: this.map.getCenter().lng()
+    })
+  }
+
+  // make an API call to get the hikes that are within the boundaries of the map
+  fetchHikesFromApi() {
+    // clear the hikes array before making the api call
+    this.setState({ hikes: [] })
+
+    // make the api call
+    $.ajax({
+      url: `http://localhost:4000/api/hikes?min_lng=${this.xMapBounds.min}&max_lng=${this.xMapBounds.max}&min_lat=${this.yMapBounds.max}&max_lat=${this.yMapBounds.min}`,
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        console.log('successful api call!');
+        console.log(data);
+        this.setState({ hikes: data })
+        // QUESTION: Should I have a message to the user appear when there are no hikes in the boundaries of the map?
+      }.bind(this), // success
+      error: function(xhr, status, err) {
+        console.log('in error');
+        console.log(err);
+        // TODO: Display error message that the api call did not work.
+      } // error
+    }); // get request
+  } // fetchHikesFromApi
+
+  // this function gets and sets map boundaries
+  getMapBounds() {
+    var mapBounds = this.map.getBounds()
+    var xMapBounds = mapBounds.b
+    var yMapBounds = mapBounds.f
+
+    this.xMapBounds.min = xMapBounds.b
+    this.xMapBounds.max = xMapBounds.f
+
+    this.yMapBounds.min = yMapBounds.f
+    this.yMapBounds.max = yMapBounds.b
+  }
+
+  render() {
+    // use this to set the center point
+    const {lat, lng} = this.state;
+    const hikes = this.state.hikes;
+    console.log('in MapComponent render');
+    console.log(lat);
+
+    // return the map
+    // the center point is defined the state
+    // By defining a GoogleMap component (from the react-google-maps library) outside of the Map component, wrapped in the withGoogleMaps method it makes it so that each time we update a components state we will only re render the components on the map and not the entire map :)
+    return(
+      <Map
+        xMapBoundsMin={this.xMapBounds.min}
+        xMapBoundsMax={this.xMapBounds.max}
+        yMapBoundsMin={this.yMapBounds.min}
+        yMapBoundsMax={this.yMapBounds.max}
+        onMapMounted={this.handleMapMounted.bind(this)}
+        handleMapChanged={this.handleMapChanged.bind(this)}
+        handleMapFullyLoaded={this.handleMapFullyLoaded.bind(this)}
+        center={{
+          lat: lat,
+          lng: lng
+        }}
+        zoom={this.zoom}
+        hikes={hikes}
+        lat={lat}
+        lng={lng}
+      />
+    )
+  }
+}
+
+export default MapContainer
