711e0b1a8328b6ee70ebe39a740d3418bb38698c
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-05 18:32:24 -0800

    form is created

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+import Link, { LinkedComponent } from 'valuelink';
+import { Input } from 'valuelink/tags';
 
-class Form extends Component {
+class Form extends LinkedComponent {
   constructor(props) {
     super(props);
 
@@ -9,59 +11,144 @@ class Form extends Component {
       description: '',
       start_lat: '',
       start_lng: '',
-    }
+      start_date: '',
+      end_date: '',
+      region: 'Central Washington',
+      miles: '',
+      elevation_gain: '',
+      max_elevation: '',
+      notes: '',
+      lakes: false,
+      coast: false,
+      rivers: false,
+      waterfalls: false,
+      fall_foliage: false,
+      wildflowers: false,
+      medows: false,
+      mountain_viwes: false,
+      summits: false,
+      established_campsites: false,
+      day_hike: false,
+      overnight: false,
+      old_growth: false,
+    };
   }
 
   static defaultProps = {
     regions: ['Central Washington', 'Eastern Washington', 'North Cascades', 'Mount Rainier Area', 'Puget Sound and Islands', 'Olympic Peninsula', 'Snoqualmie Region', 'South Cascades', 'Central Cascades', 'Issaquah Alps', 'Southwest Washington', 'Mount Adams', 'Mount Hood', 'Columbia Gorge', 'Oregon Coast', ]
   }
 
-  handleSubmit(e) {
-    //TODO: this is where I would make the api call
-    e.preventDefault();
-    console.log('in handleSubmit');
-    console.log(this.refs.name.value);
-    if (this.refs.name.value === '') {
-      // TODO: make this provide an error message on the page
-      // TODO: need to check all the other required fields to make sure data was entered!
-      alert('Name is required');
-    } else {
-      // make an api call
-      // then we need to somehow make another get request to get the new hike to show up 
-    }
+  onSubmit = e => {
+
   }
+
 
   render() {
     let regionOptions = this.props.regions.map(region => {
       return <option key={region} value="region">{region}</option>
     })
 
+    const linked = this.linkAll(); // wrap all state members in links
+
     return(
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <label>Hike name</label>
-        <input
-          type="text" placeholder='Name' ref="name" />
-        <br/>
+      <form onSubmit={this.onSubmit}>
+        <label>
+          Name: <Input valueLink={ linked.name } />
+        </label>
 
-        <label>Description</label>
-        <input type="text" placeholder='Hike description' ref="Description"/>
-        <br/>
+        <label>
+          Latitude: <Input type="number" valueLink={ linked.start_lat } />
+        </label>
 
-        <label>Latitude</label>
-        <input type="text" placeholder='Starting latitude' ref="latitude"/>
-        <br/>
+        <label>
+          Longitude: <Input type="number" valueLink={ linked.start_lng } />
+        </label>
 
-        <label>Longitude</label>
-        <input type="text" placeholder='Starting longitude' ref="longitude"/>
-        <br/>
+        <label>
+          Start date: <Input type="date" valueLink={ linked.start_date } />
+        </label>
 
-        <label>Region</label>
-        <select ref="category">
-          {regionOptions}
-        </select>
-        <br/>
+        <label>
+          End date: <Input type="date" valueLink={ linked.end_date } />
+        </label>
 
-        <input type='submit' value="submit"/>
+        <label>
+          Miles: <Input type="number" valueLink={ linked.miles } />
+        </label>
+
+        <label>
+          Elevation gain: <Input type="number" valueLink={ linked.elevation_gain } />
+        </label>
+
+        <label>
+          Max elevation: <Input type="number" valueLink={ linked.max_elevation } />
+        </label>
+
+        <label>
+          Description: <Input valueLink={ linked.description } />
+        </label>
+
+        <label>
+          Notes: <Input valueLink={ linked.notes } />
+        </label>
+
+        <label>
+          Lakes: <Input type="checkbox" valueLink={ linked.lakes } />
+        </label>
+
+        <label>
+          Lakes: <Input type="checkbox" valueLink={ linked.lakes } />
+        </label>
+
+        <label>
+          Coast: <Input type="checkbox" valueLink={ linked.coast } />
+        </label>
+
+        <label>
+          Rivers: <Input type="checkbox" valueLink={ linked.rivers } />
+        </label>
+
+        <label>
+          Waterfalls: <Input type="checkbox" valueLink={ linked.waterfalls } />
+        </label>
+
+        <label>
+          Fall foliage: <Input type="checkbox" valueLink={ linked.fall_foliage } />
+        </label>
+
+        <label>
+          Wildflowers: <Input type="checkbox" valueLink={ linked.wildflowers } />
+        </label>
+
+        <label>
+          Medows: <Input type="checkbox" valueLink={ linked.medows } />
+        </label>
+
+        <label>
+          Old-growth: <Input type="checkbox" valueLink={ linked.old_growth } />
+        </label>
+
+        <label>
+          Mountain views: <Input type="checkbox" valueLink={ linked.mountain_viwes } />
+        </label>
+
+        <label>
+          Summits: <Input type="checkbox" valueLink={ linked.summits } />
+        </label>
+
+        <label>
+          Established campsites: <Input type="checkbox" valueLink={ linked.established_campsites } />
+        </label>
+
+        <label>
+          Day hike: <Input type="checkbox" valueLink={ linked.day_hike } />
+        </label>
+
+        <label>
+          Overnight: <Input type="checkbox" valueLink={ linked.overnight } />
+        </label>
+
+        <button type='submit'>Submit</button>
 
       </form>
     ) // return
