cb03ece8ef2d9f639e51a78303bbf13288d3bb8c
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-06 19:03:34 -0800

    made AddHike component which returns a button and render it in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Map from './components/Map';
 import Form from './components/Form';
+import { AddHike } from './components/AddHike';
 import './App.css';
 
 
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <AddHike />
         <Form />
         <Map />
       </div>
---
+++ src/components/AddHike.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export class AddHike extends Component {
+  render() {
+    return (
+      <button>AddHike</button>
+    )
+  }
+
+} // AddHike
