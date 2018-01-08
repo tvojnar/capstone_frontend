30a66d87b36c6b560c217314a6740ab45c55f2a3
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-08 10:53:34 -0800

    made a HikeLocation component which is rendered from the HikeAttributes component

--- src/components/HikeAttributes.js
+++ src/components/HikeAttributes.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { HikeLocation } from './HikeLocation';
 
-
+// use this component to render the Location, Length, and Elevation components
 export class HikeAttributes extends Component {
   render() {
-    return <p>HikeAttributes</p>
+    return (
+    <div>
+      <p>HikeAttributes</p>
+      <HikeLocation />
+    </div>
+    )
   } // render
 } // HikeAttributes
---
+++ src/components/HikeLocation.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export class HikeLocation extends Component {
+  render() {
+    return <p>Location</p>
+  } // render
+} // HikeLocation
