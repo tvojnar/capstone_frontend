26f1eac88d3aae2bd7aa5179a03b6127daccc908
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-07 20:53:58 -0800

    clicking on the 'hike details' button in the info window opens the HikeDetailsModal component

--- src/components/HikeInfoWindow.js
+++ src/components/HikeInfoWindow.js
@@ -1,21 +1,46 @@
-import React, { Component } from 'react'
-import { InfoWindow } from 'react-google-maps'
+import React, { Component } from 'react';
+import { InfoWindow } from 'react-google-maps';
+import HikeDetailsModal from '../modal/HikeDetailsModal';
 
 // component to display an info window when a hike pin is clicked on
 export class HikeInfoWindow extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showHikeDetailsModal: false,
+    }
+    this.handleClick = this.handleClick.bind(this);
+  } // constructor
+
+  handleClick() {
+    this.setState({showHikeDetailsModal: true})
+  }
+
   render() {
     // set the description and name to display from the props passed in by HikeMarker
     const description = this.props.hikeData.description
     const name = this.props.hikeData.name
+
+    let hikeDetailsModal;
+    if (this.state.showHikeDetailsModal) {
+      hikeDetailsModal = <HikeDetailsModal hikeData={this.props.hikeData} />
+    }
+
     return(
       // closeWindow function is defined in HikeMarker component, which toggles showToolip to false
       // I think onCloseClick is a specific InfoWindow function that is for when the x is clicked
-      <InfoWindow onCloseClick={this.props.closeWindow}>
-        <div>
-          <h1>{name}</h1>
-          <p>{description}</p>
-        </div>
-      </InfoWindow>
+      <div>
+        { hikeDetailsModal }
+
+        <InfoWindow onCloseClick={this.props.closeWindow}>
+          <div>
+            <h1>{name}</h1>
+            <p>{description}</p>
+            <button onClick={this.handleClick}>Trip details</button>
+          </div>
+        </InfoWindow>
+      </div>
     );
   }
 }
