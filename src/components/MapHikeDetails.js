839d697536f255c2c5e66e11880f92186b026200
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-08 17:49:19 -0800

    before refactoring MapHikeDetails not to be a child class of BaseMap

--- src/components/MapHikeDetails.js
+++ src/components/MapHikeDetails.js
@@ -1,71 +1,71 @@
 
-import React from 'react';
-import { BaseMap } from './BaseMap';
-import $ from 'jquery';
-import {SingleHikeMap} from './SingleHikeMap';
+// import React from 'react';
+// import { BaseMap } from './BaseMap';
+// import $ from 'jquery';
+// import {SingleHikeMap} from './SingleHikeMap';
 
-// Map is a child class of BaseMap. BaseMap has all of the methods for reloading the map on zoom and drag movements
-// MapHikeDetails is rendered on the HikeDetailsModal
-// MapHikeDetails returns a SingleHikeMap which will be a map with a pin for the hike that is being shown in the HikeDetailsModal
- class MapHikeDetails extends BaseMap {
-  constructor(props) {
-    super(props)
-
-    // set zoom to be closer in so I can see more details about where the hike is
-    this.zoom = 11;
-
-    // Set state for lat and lng based on the start_lat and start_lng from props
-    // this lat and lng is used to set the center point of the map
-    this.state = {
-      lat: this.props.lat,
-      lng: this.props.lng,
-    }; // state
-  }  // constructor
-
-
-  // TODO: might need to customize this method once I am displaying trackpoint for the hike :)
-  // called when the maps boundaries have changed
-  // handleMapChanged() {
-  //   super.handleMapChanged()
-  //   // TODO: when I am getting trackpoints from the API I will want them to dynamically show up as the map is dragged and dropped, so I might want to do an api call to get them? Or I will just pass all the trackpoints from props to this component and display them on the map.....
-  //   // this.fetcTrackPointsFromApi();
-  // } // handleMapChanged
-
-
-  render() {
-
-    // use this to set the center point
-    const {lat, lng} = this.state;
-
-
-    // return the map
-    // the center point is defined the state
-    // By defining a GoogleMap component (from the react-google-maps library) outside of the Map component, wrapped in the withGoogleMaps method it makes it so that each time we update a components state we will only re render the components on the map and not the entire map :)
-    return(
-
-      <div style={{width: `750x`, height: `300px`}}>
-
-      <SingleHikeMap
-      onMapMounted={this.handleMapMounted.bind(this)}
-      handleMapChanged={this.handleMapChanged.bind(this)}
-      handleMapFullyLoaded={this.handleMapFullyLoaded.bind(this)}
-      center={{
-        lat: lat,
-        lng: lng
-      }}
-      zoom={this.zoom}
-      containerElement={
-        <div style={{ height: `100%` }} />
-      }
-      mapElement={
-        <div style={{ height: `100%` }} />
-      }
-      lat={lat}
-      lng={lng}
-      />
-      </div>
-    ); // return
-  } // render
-} // Map
-
-export default MapHikeDetails
+// // Map is a child class of BaseMap. BaseMap has all of the methods for reloading the map on zoom and drag movements
+// // MapHikeDetails is rendered on the HikeDetailsModal
+// // MapHikeDetails returns a SingleHikeMap which will be a map with a pin for the hike that is being shown in the HikeDetailsModal
+//  class MapHikeDetails extends BaseMap {
+//   constructor(props) {
+//     super(props)
+//
+//     // set zoom to be closer in so I can see more details about where the hike is
+//     this.zoom = 11;
+//
+//     // Set state for lat and lng based on the start_lat and start_lng from props
+//     // this lat and lng is used to set the center point of the map
+//     this.state = {
+//       lat: this.props.lat,
+//       lng: this.props.lng,
+//     }; // state
+//   }  // constructor
+//
+//
+//   // TODO: might need to customize this method once I am displaying trackpoint for the hike :)
+//   // called when the maps boundaries have changed
+//   // handleMapChanged() {
+//   //   super.handleMapChanged()
+//   //   // TODO: when I am getting trackpoints from the API I will want them to dynamically show up as the map is dragged and dropped, so I might want to do an api call to get them? Or I will just pass all the trackpoints from props to this component and display them on the map.....
+//   //   // this.fetcTrackPointsFromApi();
+//   // } // handleMapChanged
+//
+//
+//   render() {
+//
+//     // use this to set the center point
+//     const {lat, lng} = this.state;
+//
+//
+//     // return the map
+//     // the center point is defined the state
+//     // By defining a GoogleMap component (from the react-google-maps library) outside of the Map component, wrapped in the withGoogleMaps method it makes it so that each time we update a components state we will only re render the components on the map and not the entire map :)
+//     return(
+//
+//       <div style={{width: `750x`, height: `300px`}}>
+//
+//       <SingleHikeMap
+//       onMapMounted={this.handleMapMounted.bind(this)}
+//       handleMapChanged={this.handleMapChanged.bind(this)}
+//       handleMapFullyLoaded={this.handleMapFullyLoaded.bind(this)}
+//       center={{
+//         lat: lat,
+//         lng: lng
+//       }}
+//       zoom={this.zoom}
+//       containerElement={
+//         <div style={{ height: `100%` }} />
+//       }
+//       mapElement={
+//         <div style={{ height: `100%` }} />
+//       }
+//       lat={lat}
+//       lng={lng}
+//       />
+//       </div>
+//     ); // return
+//   } // render
+// } // Map
+//
+// export default MapHikeDetails
