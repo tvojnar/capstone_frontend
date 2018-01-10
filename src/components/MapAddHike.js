24dcc7f6e16d96eec6a7cc197b3cc8d9348c5073
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-10 09:33:55 -0800

    created SetPinMap, SetPinMarker, and AddHikeMap components and rendered them through AddHikeModal. Now I am closing this branch and creating a new one for adding geocoding functionality to set the location of the hike

---
+++ src/components/MapAddHike.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import SetPinMarker from './SetPinMarker';
+import { SetPinMap} from './SetPinMap';
+
+
+// renders the map for the HikeDetailsModal
+// this map doesn't need all the extra methods of Map.js because it is a stationary pin, and when I add the trackpoint that should be stationary too
+export class MapAddHike extends Component {
+  constructor(props) {
+    super(props)
+
+    // set the zoom a little closer so we can see more details around the hike
+    this.zoom = 11
+  }
+
+
+  render() {
+    // use the lat and lng  passed as props from MapHikeDetails to set the center point of the SingleHikeMap
+    const lat = this.props.lat;
+    const lng = this.props.lng;
+
+    // pass the SingleHikeMap an instance of SingleHikeMap to render on the map
+    const hike = <SetPinMarker lat={lat} lng={lng}/>
+
+    // return an instance of SingleHikeMap, which is the map that will be rendered on the HikeDetailsModal
+    return(
+      <div style={{width: `750px`, height: `400px`}}>
+        <SetPinMap
+          center={{
+            lat: lat,
+            lng: lng
+          }}
+          zoom={this.zoom}
+          containerElement={
+            <div style={{ height: `100%` }} />
+          }
+          mapElement={
+            <div style={{ height: `100%` }} />
+          }
+          hike={hike}
+        />
+      </div>
+    );
+  }
+}
+
+export default MapAddHike
