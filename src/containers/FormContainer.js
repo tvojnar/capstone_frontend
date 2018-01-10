e638e872d9c803013f792ca30e9d612319ee8da3
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-10 13:12:21 -0800

    can make successful API call to geocode a name

---
+++ src/containers/FormContainer.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react';
+import $ from 'jquery';
+import Form from '../components/Form';
+import { MapAddHike } from '../components/MapAddHike';
+import SetPinForm from '../components/SetPinForm';
+
+
+
+
+// FormContainer is rendered by AddHikeModal and HikeDetailsModal
+// FormContainer renders all of the components that make up the form to add or edit a hike
+class FormContainer extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      lat: 46.6062,
+      lng: -122.3321,
+      enteredName: false,
+      hikeName: '',
+    };
+
+
+    this.setName = this.setName.bind(this);
+  }
+
+  // this function is called by SetPinForm when it is submitted
+  setName(name) {
+    console.log('in setName and name is:');
+    console.log(name);
+    // this will update this.state.name is Form since hikeName is passed to Form via props
+    this.setState({hikeName: name})
+    this.getCoordinates(name);
+  }
+
+
+  getCoordinates(hikeName) {
+    const baseUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+    const apiKey = process.env.REACT_APP_GOOGLE_MAPS_KEY
+    const url = baseUrl + hikeName + apiKey
+    const encodedUrl = encodeURI(url);
+
+    console.log('encoded url: ');
+    console.log(encodedUrl);
+
+
+    $.ajax({
+      url: encodedUrl,
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        console.log('successful api call to geocode!');
+        console.log(data);
+      }.bind(this), // success
+      error: function(xhr, status, err) {
+        console.log('in error');
+        console.log(err);
+        // TODO: Display error message that the api call did not work.
+      } // error
+    }); // get request
+
+
+  } // getCoordinates
+// pass the Form a method to close the AddFormModal via props (hideFormModal)
+// pass Form fetchHikes so that it can call the Map components fetchHikesFromApi function when the form is submitted
+render() {
+  console.log('in render for MapAddModal and name is:');
+  console.log(this.state.hikeName);
+  return (
+    <div>
+    <SetPinForm setName={this.setName}/>
+    <MapAddHike
+    lat={this.state.lat}
+    lng={this.state.lng}
+    enteredName={this.state.enteredName}/>
+    <Form
+    hideFormModal={this.props.hideModalFromAddHike}
+    fetchHikes={this.props.fetchHikes}
+    hikeName={this.state.hikeName}
+    />
+    </div>
+  );
+}
+}
+
+export default FormContainer
--- src/modal/AddHikeModal.js
+++ src/modal/AddHikeModal.js
@@ -4,6 +4,7 @@ import BaseModal from './BaseModal';
 import Form from '../components/Form';
 import {MapAddHike} from '../components/MapAddHike';
 import SetPinForm from '../components/SetPinForm';
+import FormContainer from '../containers/FormContainer'
 
 // sets the portion of the app that should be hidden
 Modal.setAppElement('#root');
@@ -25,27 +26,16 @@ class AddHikeModal extends BaseModal {
     super(props)
 
     this.state = {
-      lat: 46.6062,
-      lng: -122.3321,
       modalIsOpen: true,
-      enteredName: false,
-      hikeName: '',
     };
 
-    this.setName = this.setName.bind(this);
   }
 
-  setName(name) {
-    console.log('in setName and name is:');
-    console.log(name);
-    this.setState({hikeName: name})
-  }
 
-// pass the Form a method to close the AddFormModal via props (hideFormModal)
-// pass Form fetchHikes so that it can call the Map components fetchHikesFromApi function when the form is submitted
+
+// pass FormContainer fetchHikes so that it can pass it to Form to call the Map components fetchHikesFromApi function when the form is submitted
   render() {
-    console.log('in render for MapAddModal and name is:');
-    console.log(this.state.hikeName);
+
     return (
       <div>
         <Modal
@@ -55,16 +45,7 @@ class AddHikeModal extends BaseModal {
           style={customStyles}
           contentLabel="Example Modal"
         >
-          <SetPinForm setName={this.setName}/>
-          <MapAddHike
-              lat={this.state.lat}
-              lng={this.state.lng}
-              enteredName={this.state.enteredName}/>
-          <Form
-            hideFormModal={this.props.hideModalFromAddHike}
-            fetchHikes={this.props.fetchHikes}
-            hikeName={this.state.hikeName}
-          />
+          <FormContainer fetchHikes={this.props.handleFetchHikes} />
           <button onClick={this.props.hideModalFromAddHike}>Cancle</button>
         </Modal>
       </div>
