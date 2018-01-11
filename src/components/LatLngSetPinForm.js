5cc5a9c2074902351a0fbb5ab71f2453a775d3db
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-11 13:44:57 -0800

    added comments to LatLngSetPinForm

--- src/components/LatLngSetPinForm.js
+++ src/components/LatLngSetPinForm.js
@@ -5,13 +5,14 @@ import * as React from 'react';
 import Link, { LinkedComponent } from 'valuelink';
 import { Input } from 'valuelink/tags';
 
-
+// this is the form that the user can manually enter a lat and lng to set the pin for a hike
 class LatLngSetPinForm extends LinkedComponent {
 
   constructor(props) {
     super(props);
 
     // Set the state using the default props
+    // later state will be reset when a user types in the input fields
     this.state = this.props.initialState;
 
     this.trimState = this.trimState.bind(this);
@@ -44,12 +45,12 @@ class LatLngSetPinForm extends LinkedComponent {
     e.preventDefault();
 
 
-    // this.state.readyToSubmit is used to determine if there was a name entered before
+    // this.state.readyToSubmit is used to determine if there was a name, lat and lng entered before
     let readyToSubmit = true
 
 
-    // check that a name has been entered
-    // if not provided by the user then set the state so that the error message and styleing shows up around the  input field in the form. Also set readyToSubmit to false to that the form wont be submitted to the API
+    // check that a name, lat and lng have been entered
+    // if not provided by the user then set the state so that the error message and styleing shows up around the empty input fields in the form. Also set readyToSubmit to false to that the form wont be submitted to the API
     if (this.state.name === '' || this.state.lat === '' || this.state.lng === '') {
       console.log('in if cause something was missing');
       readyToSubmit = false
@@ -82,7 +83,7 @@ class LatLngSetPinForm extends LinkedComponent {
     }
 
     console.log(`after all the if statements readyToSubmit: ${readyToSubmit}`);
-    // Check that the form a name before you submite (readyToSubmit = true)
+    // Check that the form a name, lat and lng before you submite (readyToSubmit = true)
     // if it is then trim the leading and trailing spaces and then submit the form and call submitForm as a callback function so that I know that trimState completed before I submit the form
     // TODO: figure out why the trim function isn't trimming the strings for name, description, and notes
     if (readyToSubmit) {
@@ -91,13 +92,12 @@ class LatLngSetPinForm extends LinkedComponent {
   }
 } // onSubmit
 
-// this will call a function in FormContainer to set the name in state
-// in will also result in getCoordinates being called, which will set the state of lat and lng in FormContainer
+// this will call the setLatLng() function in FormContainer to set the name, lat, and lng in state in FormContainer. nameEntered will be toggled to true in FormContainer as well, which will trigger a pin to appear on the map and the name, start_lat, and start_lng to be filled in in Form.js (they will match the state in FormContainer for hikeName, lat and lng)
 submitForm() {
-  console.log('in LatLngSetPinForm submitform and name, lat and lng are: ');
-  console.log(this.state.name);
-  console.log(this.state.lat);
-  console.log(this.state.lng);
+  // console.log('in LatLngSetPinForm submitform and name, lat and lng are: ');
+  // console.log(this.state.name);
+  // console.log(this.state.lat);
+  // console.log(this.state.lng);
   this.props.setLatLng(this.state.name, this.state.lat, this.state.lng);
 } // submitForm
 
@@ -106,11 +106,12 @@ trimState() {
   // Add this in once I get it working in BaseForm. Maybe make this function in a module that I can include here as well as in BaseForm?
 } // trimState
 
-// render the form, and when needed a modal
+// render the form
 render() {
 
 
   // link the name field in this way so that I can show the user error messages in the form when they try to submit the form without filling out a name
+  // linking in this way links the input field with state so that when the user types the state for name is updated
   const nameLink = Link.state(this, 'name'),
   nameIsValid = nameLink.value
 
