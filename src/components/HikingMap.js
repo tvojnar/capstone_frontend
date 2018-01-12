eb74001d7da2c405e36e901d97ff38c9ea84c783
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-11 21:30:03 -0800

    updated SetPinMap to use webscript

--- src/components/HikingMap.js
+++ src/components/HikingMap.js
@@ -31,24 +31,3 @@ export const HikingMap = compose(
       }
   </GoogleMap>
 )
-
-
-
-// export const HikingMap = withScriptjs(withGoogleMap((props) =>
-//   <GoogleMap
-//   // pass the onMapMounted function as a ref in order to save a map object somewhere in our class
-//   // pass the HikeMarkerContainer all of the hike info from the API for the hike so that I can access it all in the hike details modal that will be rendered by the info window of each hike
-//   ref={props.onMapMounted}
-//   onZoomChanged={props.handleMapChanged}
-//   onDragEnd={props.handleMapChanged}
-//   onBoundsChanged={props.handleMapFullyLoaded}
-//   defaultCenter={props.center}
-//   defaultZoom={props.zoom}>{
-//       props.hikes.length > 0 && props.hikes.map(hike => (
-//         <HikeMarkerContainer key={`hike${hike.id}`}
-//                      hikeData={hike}
-//                      fetchHikes={props.fetchHikes}/>
-//       ))
-//     }
-//   </GoogleMap>
-// ));
--- src/components/SetPinMap.js
+++ src/components/SetPinMap.js
@@ -1,15 +1,44 @@
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import {HikeMarkerContainer} from '../containers/HikeMarkerContainer';
 import React from 'react';
 
-// this is the map that is rendered on the FormContainer
-// props.hike is an instance of SetPinMarker so that the map will render a marker at the location of the hike
-// this pin will only be rendered once the user has set the location of the pin via the SetPinForm or the LatLngSetPinForm
-export const SetPinMap = withGoogleMap(props => (
-  <div>
-    <GoogleMap
-      center={props.center}
-      defaultZoom={props.zoom}>
-        {props.hike}
-      </GoogleMap>
-  </div>
-));
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap} from "react-google-maps"
+
+export const SetPinMap = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyD6xT_duuOpLAzZEE-yHY1l7do810TIL_8&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    center={props.center}
+    defaultZoom={props.zoom}>
+  >
+  {props.hike}
+  </GoogleMap>
+)
+
+
+
+
+
+
+// import { withGoogleMap, GoogleMap } from 'react-google-maps';
+// import React from 'react';
+//
+// // this is the map that is rendered on the FormContainer
+// // props.hike is an instance of SetPinMarker so that the map will render a marker at the location of the hike
+// // this pin will only be rendered once the user has set the location of the pin via the SetPinForm or the LatLngSetPinForm
+// export const SetPinMap = withGoogleMap(props => (
+//   <div>
+//     <GoogleMap
+//       center={props.center}
+//       defaultZoom={props.zoom}>
+//         {props.hike}
+//       </GoogleMap>
+//   </div>
+// ));
