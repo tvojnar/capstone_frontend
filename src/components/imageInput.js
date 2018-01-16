b2c843045b452497960630564a059bf4ae60b1f3
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-15 19:39:12 -0800

    added comments to ImageInput.js

--- src/components/imageInput.js
+++ src/components/imageInput.js
@@ -13,10 +13,12 @@ export class ImageInput extends Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
+// on change set the file selected by the user to this.state.file
   handleChange(e) {
     console.log('in handleChange');
     this.setState({file:e.target.files[0]})
   }
+
 
   handleSubmit(e) {
     e.preventDefault();
@@ -25,26 +27,41 @@ export class ImageInput extends Component {
     console.log(this.state.file.name);
 
     let file = this.state.file
-
     const url = '/api/images'
 
+    const upload_image = function(url) {
+
+        console.log('in upload_image');
+        console.log(file);
+        console.log(file.type);
+        console.log(url);
+
+
+  // THIRD: upload the image file to S3 using the presigned url
+        $.ajax({
+            type : 'PUT',
+            url : url,
+            data : file,
+            processData: false,  // tell jQuery not to convert to form data
+            contentType: file.type,
+            success: function(json) { console.log('Upload complete!') },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                console.log('Upload error: ' + XMLHttpRequest);
+                console.log(errorThrown);
+                console.log(XMLHttpRequest);
+            }
+        });
+    }
+
+    // FIRST:  make a call to the rails API (Images#index) to get a presignedUrl from S3
     $.getJSON(url, {filename: file.name},
       function(data) {
         console.log('we got the url!');
         console.log(data['url']);
+        // SECOND: call upload_image, passing it the presigned url from the rails API
+        upload_image(data['url'] )
       }
     ); // getJSON
-
-  //   let form = this;
-  //   let field = $(this).find('input[name=image]');
-  //   let file = field[0].files[0];
-  //
-  //   $.getJSON('/sign/presign_url.json',
-  //   {filename: file.name},
-  //   function(data) {
-  //     upload_image(form,data['url']);
-  //   }
-  // );
 }
 render() {
   return (
