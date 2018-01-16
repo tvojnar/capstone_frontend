36350daccbf2b6b43fe049abcd9f5c30820b460e
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-15 17:54:40 -0800

    added handleChange function that sets the file to this.file

--- src/components/imageInput.js
+++ src/components/imageInput.js
@@ -5,27 +5,38 @@ export class ImageInput extends Component {
   constructor(props) {
     super(props) ;
 
+    this.state = {
+      file:null
+    }
+
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
+  handleChange(e) {
+    console.log('in handleChange');
+    this.setState({file:e.target.files[0]})
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
     console.log('in handle submit');
-    // let form = this;
-    // let field = $(this).find('input[name=image]');
-    // let file = field[0].files[0];
-    //
-    // $.getJSON('/sign/presign_url.json',
-    // {filename: file.name},
-    // function(data) {
-    //   upload_image(form,data['url']);
+    console.log(this.state.file);
+  //   let form = this;
+  //   let field = $(this).find('input[name=image]');
+  //   let file = field[0].files[0];
+  //
+  //   $.getJSON('/sign/presign_url.json',
+  //   {filename: file.name},
+  //   function(data) {
+  //     upload_image(form,data['url']);
   //   }
   // );
 }
 render() {
   return (
-  <form class="upload-form">
-  <input id="image" type="file" name="image" accept="image/x-png, image/gif, image/jpeg" />
+  <form className="upload-form">
+  <input onChange={this.handleChange} id="image" type="file" name="image" accept="image/x-png, image/gif, image/jpeg" />
   <button onClick={this.handleSubmit} type="submit">Go!</button>
   </form>
 )
