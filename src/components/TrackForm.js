2f30a8d8a1f6a270f2f94536fa41600d11d19776
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-20 20:48:39 -0800

    added comments to TrackForm and changed the input so that it will only accept .gpx files

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -22,14 +22,17 @@ export class TrackForm extends Component {
     this.handleCloseFailure = this.handleCloseFailure.bind(this);
   }
 
+// removes the error message for when a track fails to save to the api
   handleCloseFailure() {
     this.setState({trkpt_failed: false})
   }
 
+// removes the warning message for when a user clicks upload before selecting a track
   handleCloseUploadWarning() {
     this.setState({uploadWarning: false})
   }
 
+// sets this.state.file to the file the user selected in the form
   handleChange(e) {
     console.log('in handleChange of TrackForm');
     console.log(e.target.files[0]);
@@ -37,27 +40,17 @@ export class TrackForm extends Component {
   } // handleChange
 
 
-  // xmlToString(xmlData) {
-  //
-  //     let xmlString;
-  //     //IE
-  //     if (window.ActiveXObject){
-  //         xmlString = xmlData.xml;
-  //     }
-  //     // code for Mozilla, Firefox, Opera, etc.
-  //     else{
-  //         xmlString = (new XMLSerializer()).serializeToString(xmlData);
-  //     }
-  //     return xmlString;
-  // }
-
+  // submit the xml data from the form to the api when the 'upload track' button is clicked
   handleSubmit(e) {
 
+    // prevent the default behavior of the page reloading
     e.preventDefault();
     console.log('in handle submit for TrackpointForm');
     console.log(this.state.gpx_file);
 
+    // check that a file was selected before reading the file and posting the files content to the api
     if (this.state.gpx_file) {
+
       // readFile is a function that will read the gpx_file that the user uploaded so that I can access the xml that is within the file.
       // the xml will be passed to the callback function where I will be able to access it via e.target.result
       const readFile = (file, callback) => {
@@ -76,27 +69,30 @@ export class TrackForm extends Component {
       }
 
       // readFile will be called and passed the gpx file. The callback function to readFile will call submitXml, which will make an api call to post the xml from the file (as a string) to the api. Then in the API I will use nokogiri to parse the xml to get the data I need out of it
+      // e.target.result is the xml from the file in a string
       readFile(this.state.gpx_file, function(e) {
         submitXml(e.target.result)
       });
 
 
-
+      // function to submit the xml data from the file to the API
+      // submitXml is the callback function to readFile
       const submitXml = (xml) => {
+        // get the hike id that was passed via props from HikeDetails modal
         let hikeId = this.props.id
+        // since I am using a nested rout on the backend the url is '/api/hikes/:hike_id/trackpoints'
+        // I am making the url in this way because I can only get my post to work when i send 'data: xml' and if I do 'data: {gpx_file: xml, id: hikeId}' it doesn't work, so I am using a nested route in my api for trackpoints#create so that I can send the hikeId in the url to the API instead
         const baseUrl = '/api/hikes/'
         const url = baseUrl + `${hikeId}` + '/trackpoints'
 
+        // make a post request with the xml from the file
         $.ajax({
           type : "POST",
           url : url,
-          // data: {gpx_file: xml, hikeId: hikeId},
           data : xml,
-          // dataType: "xml",
-          // contentType: "text/xml",
           processData: false,  // tell jQuery not to convert to form data
-          // headers: { 'Content-Type': file.type},
           beforeSend: function() {
+            // change the state so that the loading message will show up and all previous error and warning messages dissapear
             this.setState({
               showLoading: true,
               uploadWarning: false,
@@ -104,14 +100,17 @@ export class TrackForm extends Component {
               })
           }.bind(this),
           complete: function() {
+            // change the state so that the loading message is removed from the page
             this.setState({showLoading: false})
           }.bind(this),
           success: function(data) {
             console.log('trackpoints uploaded!');
             console.log(data);
             if (data.status == 'all') {
+                  // if the api call was a success and the api was able to create all of the trackpoints in the xml file then call a function so that the HikeDetailsModal will fetch the hikes details from the API again so that it will now have all of the trackpoints of the hike so that they can be displayed on the map
               this.props.fetchHikeDetails();
             } else {
+              // if the trackpoints did not all save then tell the user that the upload failed
               this.setState({trkpt_failed: true})
             }
           }.bind(this),
@@ -119,48 +118,22 @@ export class TrackForm extends Component {
             console.log('Error uploading trackpoints:' + XMLHttpRequest);
             console.log(errorThrown);
             console.log(XMLHttpRequest);
+            // if the api post failed then report that the upload failed to the user
             this.setState({trkpt_failed: true})
           }.bind(this)
         }); // ajax
-      }
-
-
-    }
+      } // submitXml
+    } // if
 
     if (this.state.gpx_file === null) {
+      // if the user hit upload without first selecting a file, change the state so that a warning message is shown to them that they need to choose a file before uploading
       console.log('in if in submit');
       this.setState({uploadWarning: true})
-    }
-
-
-    // let file = {gpx_track: this.state.gpx_file}
-    //debugger
-    // let file = (new XMLSerializer()).serializeToString(this.state.gpx_track)
-    // console.log('file is:');
-    // console.log(file);
-    // const url = '/api/trackpoints'
-
-    // $.ajax({
-    //     type : "POST",
-    //     url : url,
-    //     data : file,
-    //     // dataType: "xml",
-    //     // contentType: "text/xml",
-    //     processData: false,  // tell jQuery not to convert to form data
-    //     // headers: { 'Content-Type': file.type},
-    //     success: function(data) {
-    //       console.log('trackpoints uploaded!');
-    //       console.log(data);
-    //     },
-    //     error: function (XMLHttpRequest, textStatus, errorThrown) {
-    //         console.log('Error uploading trackpoints:' + XMLHttpRequest);
-    //         console.log(errorThrown);
-    //         console.log(XMLHttpRequest);
-    //     }
-    // });
+    } // if
   } // handleSubmit
 
   render() {
+    // if the api request failed or if all the trackpoints from the file were not created by the api then show them an error message
     let errorMessage;
     if (this.state.trkpt_failed) {
       errorMessage = <Callout color={Colors.ALERT}>
@@ -169,6 +142,7 @@ export class TrackForm extends Component {
       </Callout>
     } // if
 
+    // show a message telling the user that the track is loading while the POST request is processing
     let loadingMessage;
     if (this.state.showLoading) {
       loadingMessage = <Callout color={Colors.PRIMARY}>
@@ -176,6 +150,7 @@ export class TrackForm extends Component {
       </Callout>
     } // if
 
+  // if the user hit upload without first selecting a file, show them an error message that they need to choose a file before uploading
     let uploadMessage;
     if (this.state.uploadWarning) {
       uploadMessage =
@@ -188,7 +163,7 @@ export class TrackForm extends Component {
       <form>
       <p>Upload a gpx track for this hike:</p>
       {errorMessage}
-      <input onChange={this.handleChange} id="gpx" type="file" name="gpx"/>
+      <input onChange={this.handleChange} id="gpx" type="file" name="gpx" accept=".gpx"/>
       {loadingMessage}
       {uploadMessage}
       <Button onClick={this.handleSubmit} type="submit">Upload track</Button>
