27fb5af52d20cf67d070ce0afb06d37385b6bdbd
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-20 20:07:58 -0800

    made a message appear if the user clicks upload without selecting a gpx track

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 import XMLParser from 'react-xml-parser'
 import '../foundation.css';
-import {Callout, Colors} from 'react-foundation';
+import {Callout, Colors, Button} from 'react-foundation';
 import '../App.css';
 
 export class TrackForm extends Component {
@@ -11,7 +11,8 @@ export class TrackForm extends Component {
 
     this.state = {
       gpx_file:null,
-      trkpt_failed:false,
+      trkpt_failed: false,
+      uploadWarning: false,
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -46,79 +47,78 @@ export class TrackForm extends Component {
     console.log('in handle submit for TrackpointForm');
     console.log(this.state.gpx_file);
 
+    if (this.state.gpx_file) {
+      // readFile is a function that will read the gpx_file that the user uploaded so that I can access the xml that is within the file.
+      // the xml will be passed to the callback function where I will be able to access it via e.target.result
+      const readFile = (file, callback) => {
+        var reader = new FileReader()
+        reader.onloadstart = (file) => {
+          console.log("reading started")
+        }
 
-    // readFile is a function that will read the gpx_file that the user uploaded so that I can access the xml that is within the file.
-    // the xml will be passed to the callback function where I will be able to access it via e.target.result
-    const readFile = (file, callback) => {
-      var reader = new FileReader()
-      reader.onloadstart = (file) => {
-        console.log("reading started")
+        reader.onloadend = (file) => {
+          console.log("reading ended")
+          // NOTE: this.result is the xml i want
+        }
+
+        reader.onload = callback
+        reader.readAsText(this.state.gpx_file)
       }
 
-      reader.onloadend = (file) => {
-        console.log("reading ended")
-        // NOTE: this.result is the xml i want
-      }
-
-      reader.onload = callback
-      reader.readAsText(this.state.gpx_file)
-    }
-
-    // readFile will be called and passed the gpx file. The callback function to readFile will call submitXml, which will make an api call to post the xml from the file (as a string) to the api. Then in the API I will use nokogiri to parse the xml to get the data I need out of it
-    readFile(this.state.gpx_file, function(e) {
-      submitXml(e.target.result)
-    });
-
-
-
-    const submitXml = (xml) => {
-      let hikeId = this.props.id
-      const baseUrl = '/api/hikes/'
-      const url = baseUrl + `${hikeId}` + '/trackpoints'
-
-
-      $.ajax({
-        type : "POST",
-        url : url,
-        // data: {gpx_file: xml, hikeId: hikeId},
-        data : xml,
-        // dataType: "xml",
-        // contentType: "text/xml",
-        processData: false,  // tell jQuery not to convert to form data
-        // headers: { 'Content-Type': file.type},
-        beforeSend: function() {
-          this.setState({showLoading: true})
-        }.bind(this),
-        complete: function() {
-          this.setState({showLoading: false})
-        }.bind(this),
-        success: function(data) {
-          console.log('trackpoints uploaded!');
-          console.log(data);
-          if (data.status == 'all') {
-            this.props.fetchHikeDetails();
-          } else {
-            this.setState({trkpt_failed: true})
-          }
-        }.bind(this),
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-          console.log('Error uploading trackpoints:' + XMLHttpRequest);
-          console.log(errorThrown);
-          console.log(XMLHttpRequest);
-          this.setState({trkpt_failed: true})
-        }.bind(this)
+      // readFile will be called and passed the gpx file. The callback function to readFile will call submitXml, which will make an api call to post the xml from the file (as a string) to the api. Then in the API I will use nokogiri to parse the xml to get the data I need out of it
+      readFile(this.state.gpx_file, function(e) {
+        submitXml(e.target.result)
       });
+
+
+
+      const submitXml = (xml) => {
+        let hikeId = this.props.id
+        const baseUrl = '/api/hikes/'
+        const url = baseUrl + `${hikeId}` + '/trackpoints'
+
+        $.ajax({
+          type : "POST",
+          url : url,
+          // data: {gpx_file: xml, hikeId: hikeId},
+          data : xml,
+          // dataType: "xml",
+          // contentType: "text/xml",
+          processData: false,  // tell jQuery not to convert to form data
+          // headers: { 'Content-Type': file.type},
+          beforeSend: function() {
+            this.setState({showLoading: true})
+          }.bind(this),
+          complete: function() {
+            this.setState({showLoading: false})
+          }.bind(this),
+          success: function(data) {
+            console.log('trackpoints uploaded!');
+            console.log(data);
+            if (data.status == 'all') {
+              this.props.fetchHikeDetails();
+            } else {
+              this.setState({trkpt_failed: true})
+            }
+          }.bind(this),
+          error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.log('Error uploading trackpoints:' + XMLHttpRequest);
+            console.log(errorThrown);
+            console.log(XMLHttpRequest);
+            this.setState({trkpt_failed: true})
+          }.bind(this)
+        }); // ajax
+      }
+
+
+    }
+
+    if (this.state.gpx_file === null) {
+      console.log('in if in submit');
+      this.setState({uploadWarning: true})
     }
 
 
-    const parseXml = (result) => {
-      // const XMLParser = require('react-xml-parser');
-      debugger
-      const xml = new XMLParser().parseFromString(result);    // Assume xmlText contains the example XML
-      console.log(xml);
-      console.log('in parseXml');
-      // console.log(xml.getElementsByTagName('Name'));
-    }
     // let file = {gpx_track: this.state.gpx_file}
     //debugger
     // let file = (new XMLSerializer()).serializeToString(this.state.gpx_track)
@@ -156,15 +156,26 @@ export class TrackForm extends Component {
 
     let loadingMessage;
     if (this.state.showLoading) {
-      loadingMessage = <p>loading gps track ...</p>
+      loadingMessage = <Callout color={Colors.PRIMARY}>
+      <p>Loading your gpx track ...</p>
+      </Callout>
     } // if
+
+    let uploadMessage;
+    if (this.state.uploadWarning) {
+      uploadMessage =
+      <Callout color={Colors.WARNING}>
+      <p>Please choose a gpx track before uploading!</p>
+      </Callout>
+    }
     return (
       <form>
       <p>Upload a gpx track for this hike:</p>
       {errorMessage}
       <input onChange={this.handleChange} id="gpx" type="file" name="gpx"/>
       {loadingMessage}
-      <button onClick={this.handleSubmit} type="submit">Go!</button>
+      {uploadMessage}
+      <Button onClick={this.handleSubmit} type="submit">Upload track</Button>
       </form>
     )
   }
