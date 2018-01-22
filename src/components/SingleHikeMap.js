325bf0b2f05202dfceccf40949ad8adba1a74f57
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-21 17:00:51 -0800

    added close button to warning that appears when no hike is found for a name in FormContainer

--- src/components/SingleHikeMap.js
+++ src/components/SingleHikeMap.js
@@ -5,7 +5,7 @@ import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Polygon, Polyline} from "react-google-maps"
 
 // props.hike is the Marker for the hike
-// the polyline will be the gps track of the hike if it has one
+// the polyline will be the gps track of the hike if it has one 
 export const SingleHikeMap = compose(
   withProps({
     // googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDz5xfxr2GdFNz--4JKd5zfkPSmJFt6jas&libraries=geometry,drawing,places",
